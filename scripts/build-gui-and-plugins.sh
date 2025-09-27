4633b7b17f4323a94640f729991ce82c7e65474c
mcstoer <49734282+mcstoer@users.noreply.github.com>
2025-09-26 17:26:18 -0700

    Improve the scripts by adding a new line when going to the next part of the project, and also add braces around variables.

--- scripts/build-gui-and-plugins.sh
+++ scripts/build-gui-and-plugins.sh
@@ -2,46 +2,51 @@
 
 FULL_PATH=$(dirname $0)
 
-source $FULL_PATH/directories.sh
-$FULL_PATH/setup-node.sh
+source ${FULL_PATH}/directories.sh
+${FULL_PATH}/setup-node.sh
 
 # Build the desktop GUI
 (
-  export NODE_OPTIONS=--openssl-legacy-provider
+  echo ""
   echo "Building Verus-Desktop-GUI..."
-  cd $GUI_DIR
+  export NODE_OPTIONS=--openssl-legacy-provider
+  cd ${GUI_DIR}
   yarn build
 )
 
 # Build and copy the login consent client if it exists
-if [ -d "$LOGIN_CONSENT_CLIENT_DIR" ]; then
+if [ -d "${LOGIN_CONSENT_CLIENT_DIR}" ]; then
+  echo ""
   echo "Building verus-login-consent-client..."
   (
-    cd $LOGIN_CONSENT_CLIENT_DIR
+    cd ${LOGIN_CONSENT_CLIENT_DIR}
     yarn build
   )
 
   # Copy the build files over
+  echo ""
   echo "Copying the build of verus-login-consent-client..."
-  if [ ! -d "$LOGIN_CONSENT_CLIENT_PLUGIN_DIR" ]; then
-    mkdir -p $LOGIN_CONSENT_CLIENT_PLUGIN_DIR
+  if [ ! -d "${LOGIN_CONSENT_CLIENT_PLUGIN_DIR}" ]; then
+    mkdir -p ${LOGIN_CONSENT_CLIENT_PLUGIN_DIR}
   fi
-  cp -r $LOGIN_CONSENT_CLIENT_DIR/build/* $LOGIN_CONSENT_CLIENT_PLUGIN_DIR
+  cp -r ${LOGIN_CONSENT_CLIENT_DIR}/build/* ${LOGIN_CONSENT_CLIENT_PLUGIN_DIR}
 fi
 
 # Build and copy the pbaas visualizer if it exists
-if [ -d "$PBAAS_VISUALIZER_DIR" ]; then
+if [ -d "${PBAAS_VISUALIZER_DIR}" ]; then
+  echo ""
   echo "Building verus-pbaas-visualizer..."
   (
     export NODE_OPTIONS=--openssl-legacy-provider
-    cd $PBAAS_VISUALIZER_DIR
+    cd ${PBAAS_VISUALIZER_DIR}
     yarn build
   )
 
   # Copy the build files over
+  echo ""
   echo "Copying the build of verus-pbaas-visualizer..."
-  if [ ! -d "$PBAAS_VISUALIZER_PLUGIN_DIR" ]; then
-    mkdir -p $PBAAS_VISUALIZER_PLUGIN_DIR
+  if [ ! -d "${PBAAS_VISUALIZER_PLUGIN_DIR}" ]; then
+    mkdir -p ${PBAAS_VISUALIZER_PLUGIN_DIR}
   fi
-  cp -r $PBAAS_VISUALIZER_DIR/build/* $PBAAS_VISUALIZER_PLUGIN_DIR
+  cp -r ${PBAAS_VISUALIZER_DIR}/build/* ${PBAAS_VISUALIZER_PLUGIN_DIR}
 fi
--- scripts/create-build-win.sh
+++ scripts/create-build-win.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-FULL_PATH=$(dirname $0)
+FULL_PATH=$(dirname "${0}")
 
-$FULL_PATH/setup-node.sh
+${FULL_PATH}/setup-node.sh
 
-$FULL_PATH/build-gui-and-plugins.sh
+${FULL_PATH}/build-gui-and-plugins.sh
 
 # Build the the electron app
 echo "Building Verus-Desktop for Windows..."
