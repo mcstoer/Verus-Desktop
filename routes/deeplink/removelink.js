e02dcde8b33bb8ccca3bff41057cab21f87f3bd4
mcstoer <49734282+mcstoer@users.noreply.github.com>
2025-12-16 17:03:09 -0800

    Add the verus:// protocol scheme for deeplinking.

--- routes/deeplink/removelink.js
+++ routes/deeplink/removelink.js
@@ -1,7 +1,15 @@
 const { WALLET_VDXF_KEY } = require("verus-typescript-primitives")
 
 function removelink(app) {
-  return app.removeAsDefaultProtocolClient(WALLET_VDXF_KEY.vdxfid)
+  const protocols = [WALLET_VDXF_KEY.vdxfid, 'verus'];
+  const results = [];
+
+  for (const protocol of protocols) {
+    const res = app.removeAsDefaultProtocolClient(protocol);
+    results.push({ protocol, success: res });
+  }
+
+  return results;
 }
 
 module.exports = removelink
--- routes/deeplink/setuplink.js
+++ routes/deeplink/setuplink.js
@@ -2,17 +2,22 @@ const path = require('path')
 const { WALLET_VDXF_KEY } = require('verus-typescript-primitives');
 
 function setuplink(app) {
-  let res;
+  const protocols = [WALLET_VDXF_KEY.vdxfid, 'verus'];
+  const results = [];
 
-  if (process.defaultApp) {
-    if (process.argv.length >= 2) {
-      res = app.setAsDefaultProtocolClient(WALLET_VDXF_KEY.vdxfid, process.execPath, [path.resolve(process.argv[1])])
+  for (const protocol of protocols) {
+    let res;
+    if (process.defaultApp) {
+      if (process.argv.length >= 2) {
+        res = app.setAsDefaultProtocolClient(protocol, process.execPath, [path.resolve(process.argv[1])])
+      }
+    } else {
+      res = app.setAsDefaultProtocolClient(protocol)
     }
-  } else {
-    res = app.setAsDefaultProtocolClient(WALLET_VDXF_KEY.vdxfid)
+    results.push({ protocol, success: res });
   }
 
-  return res
+  return results;
 }
 
 module.exports = setuplink
