7f591936be62c476ed1bd0980ce40400562b7e83
mcstoer <49734282+mcstoer@users.noreply.github.com>
2026-02-13 11:25:51 -0800

    Add the signdata API endpoint and improve formatting in signdata.js.

--- routes/api/native/signdata.js
+++ routes/api/native/signdata.js
@@ -1,130 +1,83 @@
-
-module.exports = (api) => {    
+module.exports = api => {
   /**
    * Signs a message given the message, and an identity/address currently in the wallet
-   * 
+   *
    * @param {String} coin The chainTicker of the coin to make the call on
    * @param {String} address The identity or address to sign the message
    * @param {String} message The message to verify
    * @param {String} cursig The current signature if multisig
    */
-  api.native.sign_message = (
-    coin,
-    address,
-    message,
-    cursig = ""
-  ) => {
+  api.native.sign_message = (coin, address, message, cursig = '') => {
     return new Promise((resolve, reject) => {
       api.native
-        .callDaemon(
-          coin,
-          "signmessage",
-          [
-            address,
-            message,
-            cursig
-          ]
-        )
-      .then(resultObj => {
-        resolve(resultObj)
-      })
-      .catch(err => {
-        reject(err);
-      });
+        .callDaemon(coin, 'signmessage', [address, message, cursig])
+        .then(resultObj => {
+          resolve(resultObj);
+        })
+        .catch(err => {
+          reject(err);
+        });
     });
   };
 
   /**
    * Signs a file given the file, and an identity/address currently in the wallet
-   * 
+   *
    * @param {String} coin The chainTicker of the coin to make the call on
    * @param {String} address The identity or address to sign the file
    * @param {String} file The file to verify
    * @param {String} cursig The current signature if multisig
    */
-  api.native.sign_file = (
-    coin,
-    address,
-    file,
-    cursig = ""
-  ) => {
+  api.native.sign_file = (coin, address, file, cursig = '') => {
     return new Promise((resolve, reject) => {
       api.native
-        .callDaemon(
-          coin,
-          "signfile",
-          [
-            address,
-            file,
-            cursig
-          ]
-        )
-      .then(resultObj => {
-        resolve(resultObj)
-      })
-      .catch(err => {
-        reject(err);
-      });
+        .callDaemon(coin, 'signfile', [address, file, cursig])
+        .then(resultObj => {
+          resolve(resultObj);
+        })
+        .catch(err => {
+          reject(err);
+        });
     });
   };
 
   /**
    * Signs data given the parameters including an identity/address currently in the wallet
    * and the data to be signed
-   * 
+   *
    * @param {String} coin The chainTicker of the coin to make the call on
    * @param {Object} params The parameters to pass to signdata
    */
-  api.native.sign_data = (
-    coin,
-    params,
-  ) => {
+  api.native.sign_data = (coin, params) => {
     return new Promise((resolve, reject) => {
       api.native
-        .callDaemon(
-          coin,
-          "signdata",
-          [
-            params,
-          ]
-        )
-      .then(resultObj => {
-        resolve(resultObj)
-      })
-      .catch(err => {
-        reject(err);
-      });
+        .callDaemon(coin, 'signdata', [params])
+        .then(resultObj => {
+          resolve(resultObj);
+        })
+        .catch(err => {
+          reject(err);
+        });
     });
   };
 
-
   api.setPost('/native/sign_message', (req, res) => {
-    const {
-      chainTicker,
-      address,
-      data,
-      cursig
-    } = req.body;
+    const {chainTicker, address, data, cursig} = req.body;
 
     api.native
-      .sign_message(
-        chainTicker,
-        address,
-        data,
-        cursig
-      )
+      .sign_message(chainTicker, address, data, cursig)
       .then(resultObj => {
         const retObj = {
-          msg: "success",
-          result: resultObj
+          msg: 'success',
+          result: resultObj,
         };
 
         res.send(JSON.stringify(retObj));
       })
       .catch(error => {
         const retObj = {
-          msg: "error",
-          result: error.message
+          msg: 'error',
+          result: error.message,
         };
 
         res.send(JSON.stringify(retObj));
@@ -132,36 +85,48 @@ module.exports = (api) => {
   });
 
   api.setPost('/native/sign_file', (req, res) => {
-    const {
-      chainTicker,
-      address,
-      data,
-      cursig
-    } = req.body;
+    const {chainTicker, address, data, cursig} = req.body;
 
     api.native
-      .sign_file(
-        chainTicker,
-        address,
-        data,
-        cursig
-      )
+      .sign_file(chainTicker, address, data, cursig)
       .then(resultObj => {
         const retObj = {
-          msg: "success",
-          result: resultObj
+          msg: 'success',
+          result: resultObj,
         };
 
         res.send(JSON.stringify(retObj));
       })
       .catch(error => {
         const retObj = {
-          msg: "error",
-          result: error.message
+          msg: 'error',
+          result: error.message,
         };
 
         res.send(JSON.stringify(retObj));
       });
+  });
+
+  api.setPost('/native/sign_data', async (req, res) => {
+    const {chainTicker, params} = req.body;
+
+    try {
+      const resultObj = await api.native.sign_data(chainTicker, params);
+
+      const retObj = {
+        msg: 'success',
+        result: resultObj,
+      };
+
+      res.send(JSON.stringify(retObj));
+    } catch (error) {
+      const retObj = {
+        msg: 'error',
+        result: error.message,
+      };
+
+      res.send(JSON.stringify(retObj));
+    }
   });
 
   return api;
